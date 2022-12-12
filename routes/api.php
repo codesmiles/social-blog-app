fa655fcccffa2c764d0434d1cd552ca670942961
Nwogu Michael <mikedbchi@gmail.com>
2022-12-12 12:10:36 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -47,8 +47,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     // user friends
     Route::post("user/search-friends", [FriendsController::class,"searchFriends"]);
     Route::post("user/add-friend/{friend_id}", [FriendsController::class,"addFriend"]);
-
-
     Route::get("user/show-user-friends/", [FriendsController::class,"showUserFriends"]);
     Route::get("user/show-user-single-friend/{friend_id}", [FriendsController::class,"showSingleFriend"]);
 
